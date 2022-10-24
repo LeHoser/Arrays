65f96d7a8d06926fb7b684955f38247445019ff9
Nathan Lloyd <nathan.lloyd.7003@gmail.com>
2022-10-24 19:36:10 -0300

    Added fire and reload method
    added fire method
    added reload method
    added array for reloading to max ammo
    added array for multiple shots to come out at once on certain guns

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,14 +8,19 @@ namespace Arrays
 {
     internal class Program
     {
+        static int weapon;
+        static string[] weaponName = new string[5];
+        static int[] weaponShotCount = new int[5];
+
+        static int[] ammo = new int[5];
+
+        static int[] maxAmmo = new int[5];
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
 
-            int weapon = 4;
-
-            string[] weaponName;
-            weaponName = new string[5];
+            weapon = 4;
 
             weaponName[0] = "Pistol";
             weaponName[1] = "Shot Gun";
@@ -23,8 +28,11 @@ namespace Arrays
             weaponName[3] = "Laser";
             weaponName[4] = "Rocket Launcher";
 
-            int[] ammo;
-            ammo = new int[5];
+            weaponShotCount[0] = -1;
+            weaponShotCount[1] = -1;
+            weaponShotCount[2] = -3;
+            weaponShotCount[3] = -2;
+            weaponShotCount[4] = -1;
 
             ammo[0] = 6;
             ammo[1] = 2;
@@ -32,11 +40,38 @@ namespace Arrays
             ammo[3] = 25;
             ammo[4] = 4;
 
-            weapon = 2;
+            maxAmmo[0] = 6;
+            maxAmmo[1] = 2;
+            maxAmmo[2] = 50;
+            maxAmmo[3] = 25;
+            maxAmmo[4] = 4;
 
+            weapon = 4;
+            Fire();
+            Fire();
+            Fire();
+            Fire();
+
+            Console.WriteLine("Current weapon name: " + weaponName[weapon]);
+            Console.WriteLine("Weapon Ammo: " + ammo[weapon]);
+            Reload();
             Console.WriteLine("Current weapon name: " + weaponName[weapon]);
             Console.WriteLine("Weapon Ammo: " + ammo[weapon]);
             Console.ReadKey(true);
         }
+
+        static void Fire()
+        {
+            ammo[weapon] += weaponShotCount[weapon];
+        }
+
+        static void Reload()
+        {
+            ammo[weapon] = maxAmmo[weapon];
+        }
     }
+
+    
+
+    
 }
